3677f622cf657871c0d8927ed69cf7f6981825f9
Cristina Laryea <crispad06@gmail.com>
2018-07-09 18:22:57 -0400

    imported addnote.js

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+//import { AddNote } from './components/AddNote';
 
 class App extends Component {
   render() {
@@ -13,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* <Route path="/AddNote" render={(props) => <AddNote {...props} add={this.handleAdd} />}/>  */}
       </div>
     );
   }
