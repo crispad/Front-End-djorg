d69ed23abb9f97cadaa8c0fa119eded828b3295a
Cristina Laryea <crispad06@gmail.com>
2018-07-10 17:24:12 -0400

    fixed import

--- notes/src/App.js
+++ notes/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom';
-//import { AddNote } from './components/AddNote';
+import  AddNote  from './components/AddNote';
 
 class App extends Component {
   render() {
@@ -15,7 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        {/* <Route path="/AddNote" render={(props) => <AddNote {...props} add={this.handleAdd} />}/>  */}
+        <Route path="/AddNote" render={(props) => <AddNote {...props} add={this.handleAdd} />}/> 
       </div>
     );
   }
