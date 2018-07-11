faf2d071066136a086959c35d13ef3673af08827
Cristina Laryea <crispad06@gmail.com>
2018-07-11 18:29:05 -0400

    imported files to app file

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,22 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch, BrowserRouter, Redirect} from 'react-router-dom';
+
+import {Provider, connect} from "react-redux";
+import {createStore, applyMiddleware} from "redux";
+
+import thunk from "redux-thunk";
+
+import {auth} from "./actions";
+import noteApp from "./reducers";
+
+import note from "./components/note";
+import register from "./components/register";
+import login from "./components/login";
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+let store = createStore(noteApp, applyMiddleware(thunk));
+
+class RootContainerComponent extends Component {
+
+  componentDidMount() {
+    this.props.loadUser();
+  }
+
+  PrivateRoute = ({component: ChildComponent, ...rest}) => {
+    return <Route {...rest} render={props => {
+      if (this.props.auth.isLoading) {
+        return <em>Loading...</em>;
+      }else if (!this.props.auth.isAuthenticated) {
+        return <Redirect to="/login" />;
+      }else {
+        return <ChildComponent {...props} />
+      }
+     }} />
+}
+
+render() {
+  let {PrivateRoute} = this;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <PrivateRoute exact path="/" component={note} />
+        <Route exact path="/register" component={register} />
+        <Route exact path="login" component={login} />
+        </Switch>
+        </BrowserRouter>
+  );
+}
+}
+
+const mapStateToProps = state => {
+  return {
+    auth: state.auth,
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    loadUser: () => {
+      return dispatch(auth.loadUser());
+    }
+  }
+}
+
+let RootContainer = connect(mapStateToProps, mapDispatchToProps)(RootContainerComponent);
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <RootContainer />
+      </Provider>
+    )
+  }
+}
